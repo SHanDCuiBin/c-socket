3c26a5df4e795cc82b616555fb51777c5a53d91c
SHanDCuiBin <SDCuiBin@outlook.com>
2021-07-05 15:36:02 +0800

    添加项目文件。

---
+++ SocketClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketUtil.SocketClient client = new SocketUtil.SocketClient(8888);
+            client.StartClient();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketUtil.SocketServer server = new SocketUtil.SocketServer(8888);
+            server.StartListen();
+            Console.ReadKey();
+        }
+    }
+}
